5cc1d91fce2e4cd31f472538b0e7991129bee310
Possibly Mason N. <135172681+PrimeKidMN@users.noreply.github.com>
2025-02-04 23:19:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const app = express();
 
 // Set up the proxy to Krunker.io
 app.use('/', createProxyMiddleware({
-  target: 'https://krunker.io', // Target Krunker.io
-  changeOrigin: true,           // Set this to true to ensure the origin is changed
-  secure: false                 // Set to false if Krunker uses self-signed certificates (optional)
+  target: 'https://krunker.io',
+  changeOrigin: true,
+  secure: false,
+  gzip: true,  // Enable gzip compression
 }));
+
 
 // Set the port for your server
 const PORT = process.env.PORT || 8000;
